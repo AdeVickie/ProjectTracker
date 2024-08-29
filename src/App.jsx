8269e6965fbac8f7cf5fad531b521ec6263da4c1
oseji <jiade1233@gmail.com>
2024-08-29 19:40:14 +0100

    vicky

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,8 @@ import React, { useState, useEffect } from "react";
 import LoginPage from "./Component/LoginPage";
 import Dashboard from "./Component/Dashboard";
 
-// import LandingPage from "./Component/LandingPage";
-// import { elements } from "chart.js";
-
-// import {
-//   Sheet,
-//   Spreadsheet,
-//   SpreadsheetComponent,
-// } from "@syncfusion/ej2-react-spreadsheet";
-import Sheets from "./Component/Sheets";
-
-import { db, auth } from "./firebase-config/firebase";
-import { getDocs, collection } from "firebase/firestore";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
-
 const App = () => {
   const [sidebarToggle, setSidebarToggle] = useState(true);
-
-  const [admin, setAdmin] = useState(auth.currentUser?.email);
 
   const [userEmail, setUserEmail] = useState();
   const [userPassword, setUserPassword] = useState();
@@ -36,85 +16,6 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   const [dbData, setDbData] = useState([]);
-
-  const createAccount = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, userEmail, userPassword);
-
-      setIsLoggedIn(true);
-      setUserEmail("");
-      setUserPassword("");
-    } catch (err) {
-      setIsLoggedIn(false);
-    }
-  };
-
-  const signIn = async () => {
-    setIsLoginLoading(true);
-
-    try {
-      await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-      setIsLoggedIn(true);
-
-      setLoginEmail("");
-      setLoginPassword("");
-    } catch (err) {
-      console.error(err);
-
-      setIsLoggedIn(false);
-    } finally {
-      setIsLoginLoading(false);
-    }
-  };
-
-  const logOut = async () => {
-    setIsLoginLoading(true);
-
-    try {
-      await signOut(auth);
-      setIsLoggedIn(false);
-
-      setLoginEmail("");
-      setLoginPassword("");
-    } catch (err) {
-      console.error(err);
-      setIsLoggedIn(true);
-    }
-  };
-
-  const getDatabaseData = async () => {
-    const user = auth.currentUser;
-
-    try {
-      if (user) {
-        const q = collection(db, `users/${user.email}/employeeData`);
-
-        const data = (await getDocs(q)).docs;
-        const filteredData = data.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-
-        setDbData(filteredData);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // get database data and current username once logged in
-  useEffect(() => {
-    if (isLoggedIn) {
-      getDatabaseData();
-    }
-
-    if (auth.currentUser?.email) {
-      const name = auth.currentUser?.email?.split("@");
-      setAdmin(name[0]);
-    }
-
-    console.log(dbData);
-  }, [isLoggedIn]);
 
   return (
     <div className="App">
@@ -137,8 +38,6 @@ const App = () => {
           setLoginEmail={setLoginEmail}
           loginPassword={loginPassword}
           setLoginPassword={setLoginPassword}
-          signIn={signIn}
-          createAccount={createAccount}
         ></LoginPage>
       )}
     </div>
